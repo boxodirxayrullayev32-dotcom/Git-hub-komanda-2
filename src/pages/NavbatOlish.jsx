20bfb3b8d7a0c357e18d097456f5f6c4fd26bf0b
boxodirxayrullayev32-dotcom <boxodirxayrullayev32@gmail.com>
2026-06-30 19:54:14 +0500

    Mening sahifalarim va o'zgarishlarim

--- src/pages/NavbatOlish.jsx
+++ src/pages/NavbatOlish.jsx
@@ -6,7 +6,7 @@ const NavbatOlish = () => {
       <h1 className="text-3xl font-bold text-gray-800">Navbat Olish</h1>
       <p className="text-gray-600 mt-2">Navbat olish sahifasi — UI ni shu yerga yozing</p>
     </div>
-  )
+  ) 
 }
 
 export default NavbatOlish
